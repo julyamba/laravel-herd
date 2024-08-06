2e442ffa0632f5c0c06a463abcbe9313de3bccd2
julyamba <jyamz7690@gmail.com>
2024-08-06 10:23:05 +0800

    day 6

---
+++ resources/views/job.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <x-slot:heading>Job</x-slot:heading>
+
+    <p class="text-lg font-bold">{{ $job['title'] }}</p>
+    <p class="text-gray-500 text-md">Pays {{ $job['salary'] }} per year</p>
+    
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,65 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Arr;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs',[
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$50,000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Manager',
+                'salary' => '$40,000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Assistant Manager',
+                'salary' => '$30,000'
+            ],
+            [
+                'id' => 4,
+                'title' => 'Web Developer',
+                'salary' => '$25,000'
+            ],
+        ]
+    ]);
+});
+
+Route::get('/jobs/{id}', function ($id) {
+    $jobs = [
+        [
+            'id' => 1,
+            'title' => 'Director',
+            'salary' => '$50,000'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Manager',
+            'salary' => '$40,000'
+        ],
+        [
+            'id' => 3,
+            'title' => 'Assistant Manager',
+            'salary' => '$30,000'
+        ],
+        [
+            'id' => 4,
+            'title' => 'Web Developer',
+            'salary' => '$25,000'
+        ],
+    ];
+
+    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    return view('job', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
