fd48bbbbe327a3f9cbbbab1f3e4f85ce6e5225ab
julyamba <jyamz7690@gmail.com>
2024-08-08 09:55:18 +0800

    update: day 10

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Arr;
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
@@ -9,56 +9,15 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
     return view('jobs',[
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Manager',
-                'salary' => '$40,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Assistant Manager',
-                'salary' => '$30,000'
-            ],
-            [
-                'id' => 4,
-                'title' => 'Web Developer',
-                'salary' => '$25,000'
-            ],
-        ]
+        'jobs' => Job::all()
     ]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$50,000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Manager',
-            'salary' => '$40,000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Assistant Manager',
-            'salary' => '$30,000'
-        ],
-        [
-            'id' => 4,
-            'title' => 'Web Developer',
-            'salary' => '$25,000'
-        ],
-    ];
-
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    $job = Job::find($id);
+    if (!$job) {
+        abort(404);
+    }
     return view('job', ['job' => $job]);
 });
 
